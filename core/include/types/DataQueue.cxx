ce7e7da69467ac1f993346e478de7e1f2088a4b6
Robert Strickland <strickland.rbls@gmail.com>
2023-08-16 12:14:45 -0500

    Added / Fixed Threaded Extract / Read

--- core/include/types/DataQueue.cxx
+++ core/include/types/DataQueue.cxx
@@ -11,15 +11,11 @@ uint8_t& FileData::operator[](int i) {
 
 auto DataQueue::get_free_block() -> FileData* {
     if(!is_full()){
-        std::cout << "[DataQueue] Returning writable <" <<
-            &(_storage[_i_writable]) << ">\n";
         return &(_storage[_i_writable]);
     }
     return nullptr;
 }
 auto DataQueue::get_next_read() -> FileData* {
-    std::cout << "[DataQueue] Returning readable <" <<
-        &(_storage[_i_readable]) << ">\n";
     return &(_storage[_i_readable]);
 }
 void DataQueue::update_free_index(){ _i_writable = (_i_writable+1) % MAX_QUEUE_LENGTH; _size++; }
--- core/include/types/DataQueue.h
+++ core/include/types/DataQueue.h
@@ -3,8 +3,10 @@
 
 #include <cstdint>
 #include <atomic>
+#include <future>
 #include <vector>
 #include <iostream>
+#include <thread>
 
 namespace types {
 
@@ -37,21 +39,25 @@ class DataQueue {
 public:
     DataQueue(uint8_t size): 
         _size{ 0 },
-        _storage{ MAX_QUEUE_LENGTH, FileData() }
-        { 
-            std::cout << "[DataQueue] Allocation <" <<
-                &_storage[0] << " - " << 
-                &_storage[MAX_QUEUE_LENGTH -1] << ">\n";
-        }
+        _storage{ MAX_QUEUE_LENGTH }
+        { }
     
     auto get_free_block() -> FileData*;
     auto get_next_read() -> FileData*;
 
     auto is_empty() { return _size == 0; }
     auto is_full() { return _size == 10; }
-
+    
     void update_free_index();
     void update_read_index();
+
+    std::future<bool> is_readable = std::async(
+        [this](){ 
+            while(_size <= 0)
+                std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            return true;
+        }
+    );
 private:
     std::vector<FileData> _storage;
     std::atomic<uint8_t> 
